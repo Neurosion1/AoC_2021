0e859eb29ae2571bf88363148e5e30719c126f94
Neurosion1 <neurosion@yahoo.com>
2021-12-08 12:46:14 -0500

    * Day 8 Finished (famous last words)

--- AoC_8/AoC_8.cpp
+++ AoC_8/AoC_8.cpp
@@ -12,27 +12,28 @@
 
 namespace
 {
-  typedef std::string digit_desc;
-  typedef std::vector<std::pair<std::array<digit_desc, 10>, std::array<digit_desc, 4>>> Digitlist;
+  typedef std::array<std::string, 10> Signal;
+  typedef std::array<std::string, 4>  Output;
+  typedef std::vector<std::pair<Signal, Output>> Digitlist;
 
   void process_file(std::ifstream& input, Digitlist& digitlist)
   {
-    std::string delimeter;
+    std::string delimiter;
     while (!input.eof()) {
       char buf[5000];
       input.getline(buf, 5000);
       std::stringstream string_buffer(buf);
-      std::array<digit_desc, 10> signal;
-      std::array<digit_desc, 4> output;
+      Signal signal;
+      Output output;
       for (int i = 0; i < 10; ++i) {
-        digit_desc digits;
+        std::string digits;
         string_buffer >> digits;
         std::sort(digits.begin(), digits.end());
         signal[i] = digits;
       }
-      string_buffer >> delimeter;
+      string_buffer >> delimiter;
       for (int i = 0; i < 4; ++i) {
-        digit_desc digits;
+        std::string digits;
         string_buffer >> digits;
         std::sort(digits.begin(), digits.end());
         output[i] = digits;
@@ -41,96 +42,95 @@ namespace
     };
   }
 
+  // Assumes both strings are sorted.
+  bool is_subset(const std::string& test_range, const std::string& source_range)
+  {
+    for (auto source_iter = source_range.cbegin(), test_iter = test_range.cbegin();
+              test_iter != test_range.cend();
+            ++test_iter)
+    {
+      while (*source_iter != *test_iter) {
+        if (++source_iter == source_range.cend()) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
   std::unordered_map<std::string, int> decode(std::array<std::string, 10> input)
   {
     std::array<std::string, 10> results;
-    std::unordered_set<std::string> leftovers(input.begin(), input.end());
+    std::unordered_set<std::string> fives;
+    std::unordered_set<std::string> sixes;
     for (int i = 0; i < 10; ++i) {
       switch(input[i].size()) {
         case 2:
           // 1 has two segments.
           results[1] = input[i];
-          leftovers.erase(input[i]);
           break;
         case 3:
           // 7 has three segments.
           results[7] = input[i];
-          leftovers.erase(input[i]);
           break;
         case 4:
           // 4, happily, has four segments.
           results[4] = input[i];
-          leftovers.erase(input[i]);
+          break;
+        case 5:
+          fives.insert(input[i]);
+          break;
+        case 6:
+          sixes.insert(input[i]);
           break;
         case 7:
           // 8 has all 7 segments.
           results[8] = input[i];
-          leftovers.erase(input[i]);
           break;
       }
     }
-    
-    // 3 is the only remaining 5-segment number that has both segments of 1.
-    for (auto entry : leftovers) {
-      if (entry.size() == 5) {
-        std::vector<char> missing;
-        std::set_difference(results[1].begin(), results[1].end(), entry.begin(), entry.end(), std::inserter(missing, missing.begin()));
-        if (missing.empty()) {
-          results[3] = entry;
-          leftovers.erase(entry);
-          break;
-        }
-      }
-    }
-    
+
     // 9 is the only 6-segment number that has all segments of 4.
-    for (auto entry : leftovers) {
-      if (entry.size() == 6) {
-        std::vector<char> missing;
-        std::set_difference(results[4].begin(), results[4].end(), entry.begin(), entry.end(), std::inserter(missing, missing.begin()));
-        if (missing.empty()) {
-          results[9] = entry;
-          leftovers.erase(entry);
-          break;
-        }
+    for (const auto & entry : sixes) {
+      if (is_subset(results[4], entry)) {
+        results[9] = entry;
+        sixes.erase(entry);
+        break;
       }
     }
     
     // 0 is the only remaining 6-segment number that has all segments of 7.
-    for (auto entry : leftovers) {
-      if (entry.size() == 6) {
-        std::vector<char> missing;
-        std::set_difference(results[7].begin(), results[7].end(), entry.begin(), entry.end(), std::inserter(missing, missing.begin()));
-        if (missing.empty()) {
-          results[0] = entry;
-          leftovers.erase(entry);
-          break;
-        }
+    for (const auto & entry : sixes) {
+      if (is_subset(results[7], entry)) {
+        results[0] = entry;
+        sixes.erase(entry);
+        break;
       }
     }
     
     // 6 is the only remaining 6-segment number.
-    for (auto entry : leftovers) {
-      if (entry.size() == 6) {
-        results[6] = entry;
-        leftovers.erase(entry);
+    results[6] = *sixes.begin();
+
+    // 3 is the only 5-segment number that has both segments of 1.
+    for (const auto & entry : fives) {
+      if (is_subset(results[1], entry)) {
+        results[3] = entry;
+        fives.erase(entry);
         break;
       }
     }
     
-    // 5 is the only remaining number whose segments are all in 6.
-    for (auto entry : leftovers) {
-      std::vector<char> remaining_letter;
-      std::set_difference(entry.begin(), entry.end(), results[6].begin(), results[6].end(), std::inserter(remaining_letter, remaining_letter.begin()));
-      if (remaining_letter.empty()) {
+    // 5 is the only 5-segment number whose segments are all in 6.
+    for (const auto & entry : fives) {
+      if (is_subset(entry, results[6])) {
         results[5] = entry;
-        leftovers.erase(entry);
+        fives.erase(entry);
         break;
       }
     }
     
-    // Last one is 2.
-    results[2] = *leftovers.begin();
+    // Last is 2.
+    results[2] = *fives.begin();
     
     std::unordered_map<std::string, int> retval;
     for (int i = 0; i < 10; ++i) {
@@ -150,34 +150,29 @@ int main()
   }
   
   Digitlist digitlist;
-  process_file(input, digitlist);
-  
-  int part_one_count = 0;
-  for (std::pair<std::array<digit_desc, 10>, std::array<digit_desc, 4>> entry : digitlist) {
+  process_file(input, digitlist);  
+
+  long part_one_count = 0;
+  long part_two_count = 0;
+  for (auto & entry : digitlist) {
+    long multiplier = 1000;
+    auto decoded = decode(entry.first);
     for (int i = 0; i < 4; ++i) {
-      size_t entry_size = entry.second[i].size();
-      if (entry_size == 2 ||
-          entry_size == 3 ||
-          entry_size == 4 ||
-          entry_size == 7) {
+      long digit = decoded[entry.second[i]];
+      switch (digit) {
+      case 1:
+      case 4:
+      case 7:
+      case 8:
         ++part_one_count;
       }
+      part_two_count += multiplier * digit;
+      multiplier /= 10;
     }
   }
-  
+
   std::cout << "Part One: " << part_one_count << std::endl;
-  
-  long total = 0;
-  for (auto entry : digitlist) {
-    auto decoded = decode(entry.first);
-    long this_number = 0;
-    this_number += (1000 * decoded[entry.second[0]]);
-    this_number += (100 * decoded[entry.second[1]]);
-    this_number += (10 * decoded[entry.second[2]]);
-    this_number += decoded[entry.second[3]];
-    total += this_number;
-  }
-  std::cout << "Part Two: " << total << std::endl;
+  std::cout << "Part Two: " << part_two_count << std::endl;
 
   return 0;
 }

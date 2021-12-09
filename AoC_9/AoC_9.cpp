677fd9608cdfdc2ed683cafbc333eee1c1809a62
Neurosion1 <neurosion@yahoo.com>
2021-12-09 13:36:02 -0500

    * Day 9 finished, more or less

--- AoC_9/AoC_9.cpp
+++ AoC_9/AoC_9.cpp
@@ -8,6 +8,7 @@
 #include <algorithm>
 #include <numeric>
 #include <vector>
+#include <map>
 
 namespace
 {
@@ -16,16 +17,17 @@ namespace
 
   void process_file(std::ifstream& input, Grid& grid)
   {
-    char buf[5000];
-    for (int x = 0; x < 100; ++x) {
+    while (!input.eof()) {
+      char buf[5000];
       input.getline(buf, 5000);
       std::stringstream string_buffer(buf);
       std::vector<int> row;
-      for (int y = 0; y < 100; ++y) {
-        char c;
+      char c;
+      string_buffer >> c;
+      do {
+        row.push_back(c - '0');
         string_buffer >> c;
-        row.emplace_back(c - '0');
-      };
+      } while (!string_buffer.eof());
       grid.emplace_back(row);
     };
   }
@@ -37,40 +39,37 @@ namespace
     if (x > 0 && test >= grid[x-1][y]) {
       return false;
     }
-    
-    if (x < 99 && test >= grid[x+1][y]) {
+    if (x < grid.size() - 1 && test >= grid[x+1][y]) {
       return false;
     }
-    
     if (y > 0 && test >= grid[x][y-1]) {
       return false;
     }
-    
-    if (y < 99 && test >= grid[x][y+1]) {
+    if (y < grid[0].size() - 1 && test >= grid[x][y+1]) {
       return false;
     }
     
     return true;
   }
 
-  int fill(CharGrid& grid, int x, int y)
+  int fill(CharGrid& grid, int x, int y, char unfilled_char, char fill_char)
   {
     int retval = 0;
     
-    if (grid[x][y] == '?') {
+    if (grid[x][y] == unfilled_char) {
       retval = 1;
-      grid[x][y] = 'O';
-      if (x > 0 && grid[x-1][y] == '?') {
-        retval += fill(grid, x - 1, y);
+      grid[x][y] = fill_char;
+      if (x > 0) {
+        retval += fill(grid, x - 1, y, unfilled_char, fill_char);
       }
-      if (x < 99 && grid[x+1][y] == '?') {
-        retval += fill(grid, x + 1, y);
+      if (x < grid.size() - 1) {
+        retval += fill(grid, x + 1, y, unfilled_char, fill_char);
       }
-      if (y > 0 && grid[x][y-1] == '?') {
-        retval += fill(grid, x, y - 1);
+      if (y > 0) {
+        retval += fill(grid, x, y - 1, unfilled_char, fill_char);
       }
-      if (y < 99 && grid[x][y+1] == '?') {
-        retval += fill(grid, x, y + 1);
+      if (y < grid[0].size() - 1) {
+        retval += fill(grid, x, y + 1, unfilled_char, fill_char);
       }
     }
     
@@ -89,11 +88,13 @@ int main()
   Grid grid;
   process_file(input, grid);
   
+  std::vector<std::pair<int, int>> lowPoints;
   int part_one = 0;
   for (int x = 0; x < grid.size(); ++x) {
     for (int y = 0; y < grid.size(); ++y) {
       if (is_local_minimum(grid, x, y)) {
         part_one += (1 + grid[x][y]);
+        lowPoints.push_back( { x, y } );
       }
     }
   }
@@ -107,30 +108,45 @@ int main()
     }
   }
   
-  std::vector<int> areas;
-  for (int x = 0; x < chargrid.size(); ++x) {
-    for (int y = 0; y < chargrid.size(); ++y) {
-      if (chargrid[x][y] == '?') {
-        int fillsize = fill(chargrid, x, y);
-        if (fillsize > 0) {
-          areas.push_back(fillsize);
-        }
-      }
+  // Interestingly it looks like the input is designed so that every
+  // basin has exactly one low point; every basin continuously
+  // descends from '9' to '0'. There are no intemediate bowls, like
+  // in the following:
+  //
+  //    9999999
+  //    9444449
+  //    9414149 <-- Two low points within this single basin,
+  //    9444449     which doesn't seem to happen.
+  //    9999999
+  //
+  // This doesn't really affect the solution, but I imagine it might
+  // become important in a future puzzle.
+  //
+  std::multimap<int, std::pair<int,int>, std::greater<int>> areas;
+  for (auto point : lowPoints) {
+    int fillsize = fill(chargrid, point.first, point.second, '?', 'O');
+    if (fillsize > 0) {
+      areas.insert({ fillsize, point });
     }
   }
   
-  std::sort(areas.begin(), areas.end(), [](int x, int y) { return x > y; });
-  
-  for (int x = 0; x < grid.size(); ++x) {
-    for (int y = 0; y < grid.size(); ++y) {
-      if (is_local_minimum(grid, x, y)) {
-        part_one += (1 + grid[x][y]);
-      }
-    }
-  }
+  auto iter = areas.begin();
+  auto first = *iter, second = *++iter, third = *++iter;
   
   std::cout << "Part One: " << part_one << std::endl;
-  std::cout << "Part Two: " << areas[0] * areas[1] * areas[2] << std::endl;
+  std::cout << "Part Two: " << first.first * second.first * third.first << std::endl;
+  std::cout << std::endl;
+  
+  // Let's visualize it for giggles.
+  fill(chargrid, first.second.first,  first.second.second,  'O', ' ');
+  fill(chargrid, second.second.first, second.second.second, 'O', ' ');
+  fill(chargrid, third.second.first,  third.second.second,  'O', ' ');
+  for (const auto & row : chargrid) {
+    for (char c : row) {
+      std::cout << c;
+    }
+    std::cout << std::endl;
+  }
   
   return 0;
 }

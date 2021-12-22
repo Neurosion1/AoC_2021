c36eabf7e0da3c95ca1c2787e5f5841e2504a17e
Neurosion1 <neurosion@yahoo.com>
2021-12-22 14:52:59 -0500

    * Day 22 (and some day 21 cleanup)

--- AoC_21/AoC_21.cpp
+++ AoC_21/AoC_21.cpp
@@ -6,7 +6,6 @@ namespace
   class DeterministicDie
   {
   public:
-    
     DeterministicDie() : next_roll_(1), total_rolls_(0) {}
     
     long long roll()
@@ -14,30 +13,24 @@ namespace
       int total = 0;
       for (int i = 0; i < 3; ++i) {
         total += (next_roll_++);
-        if (next_roll_ > 100) {
-          next_roll_ = 1;
-        }
+        next_roll_ = (next_roll_ == 101 ? 1 : next_roll_);
       }
       total_rolls_ += 3;
       return total;
     }
     
     int total_rolls_;
-    
   private:
     long long next_roll_;
   };
 
-  int P1_START = 8, P2_START=1;
-
-  typedef std::array<std::array<std::array<std::array<long long, 21>, 21>, 10>, 10> GameState;
+  const int P1_START = 8, P2_START = 1;
 }
 
 int main(int argc, const char * argv[]) {
 
   long long p1_score = 0, p2_score = 0;
   int p1_space = P1_START, p2_space = P2_START;
-  
   DeterministicDie die;
   while (p1_score < 1000 && p2_score < 1000) {
     p1_space = (p1_space + die.roll()) % 10;
@@ -47,53 +40,52 @@ int main(int argc, const char * argv[]) {
       p2_score += (p2_space == 0 ? 10 : p2_space);
     }
   }
-  
   long long part_one_answer = (p1_score > p2_score ? p2_score : p1_score) * die.total_rolls_;
-  
   std::cout << "Part one: " << part_one_answer << "\n";
   
-  bool doing_p1 = true;
-  long long game_count = 1;
-  long long wins[2] = { 0 };
-  
+  // Count [21 potential P1 scores][21 potential P2 scores][10 P1 spaces][10 P2 spaces]
+  typedef std::array<std::array<std::array<std::array<long long, 21>, 21>, 10>, 10> GameState;
   GameState current_game_states = { 0 };
-  current_game_states[P1_START][P2_START][0][0] = 1;
+  current_game_states[P1_START][P2_START][0][0] = 1; // One start-of-game state
+  long long total_game_states = 1;
+  bool doing_p1 = true;
+  long long wins[2] = { 0 };
   do {
     GameState next_game_states = { 0 };
     for (int p1_space = 0; p1_space < 10; ++p1_space) {
       for (int p2_space = 0; p2_space < 10; ++p2_space) {
         for (int p1_score = 0; p1_score < 21; ++p1_score) {
           for (int p2_score = 0; p2_score < 21; ++p2_score) {
-            long long current_games = current_game_states[p1_space][p2_space][p1_score][p2_score];
-            if (current_games > 0) {
-              static const long long scores[7][2] = { {3, 1}, {4, 3}, {5, 6}, {6, 7}, {7, 6}, {8, 3}, {9, 1}};
+            long long these_game_states = current_game_states[p1_space][p2_space][p1_score][p2_score];
+            if (these_game_states > 0) {
+              // Values and frequencies for the 27 theoretical ways one could roll a 3-sided die 3 times
+              static const long long scores[7][2] = { {3, 1}, {4, 3}, {5, 6}, {6, 7}, {7, 6}, {8, 3}, {9, 1} };
               for (long long score = 0; score < 7; ++score) {
+                long long new_game_states = these_game_states * scores[score][1];
                 if (doing_p1) {
                   int new_space = (p1_space + scores[score][0]) % 10;
                   int new_score = (new_space == 0 ? 10 : new_space) + p1_score;
                   if (new_score >= 21) {
-                    wins[doing_p1 ? 0 : 1] += current_games * scores[score][1];
+                    wins[0] += new_game_states;
                   }
                   else {
-                    long long new_games = current_games * scores[score][1];
-                    next_game_states[new_space][p2_space][new_score][p2_score] += new_games;
-                    game_count += new_games;
+                    next_game_states[new_space][p2_space][new_score][p2_score] += new_game_states;
+                    total_game_states += new_game_states;
                   }
                 }
                 else {
                   int new_space = (p2_space + scores[score][0]) % 10;
                   int new_score = (new_space == 0 ? 10 : new_space) + p2_score;
                   if (new_score >= 21) {
-                    wins[doing_p1 ? 0 : 1] += current_games * scores[score][1];
+                    wins[1] += new_game_states;
                   }
                   else {
-                    long long new_games = current_games * scores[score][1];
-                    next_game_states[p1_space][new_space][p1_score][new_score] += new_games;
-                    game_count += new_games;
+                    next_game_states[p1_space][new_space][p1_score][new_score] += new_game_states;
+                    total_game_states += new_game_states;
                   }
                 }
               }
-              game_count -= current_games;
+              total_game_states -= these_game_states;
             }
           }
         }
@@ -102,8 +94,7 @@ int main(int argc, const char * argv[]) {
     current_game_states = next_game_states;
     doing_p1 = !doing_p1;
   }
-  while (game_count > 0);
-  
+  while (total_game_states > 0);
   std::cout << (wins[0] > wins[1] ? wins[0] : wins[1]) << "\n";
   
   return 0;
---
+++ AoC_22/AoC_22.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <fstream>
+#include <numeric>
+#include <vector>
+#include <sstream>
+#include <array>
+#include <list>
+
+namespace
+{
+  struct Range
+  {
+    long long start_x_, end_x_;
+    long long start_y_, end_y_;
+    long long start_z_, end_z_;
+  };
+
+  class Engine
+  {
+  public:
+    void subtract_segment(const Range& subtraction)
+    {
+      std::list<Range> new_segments;
+      for (auto segment = segments_.begin(); segment != segments_.end();) {
+        if (segment->start_x_ <= subtraction.end_x_   &&
+            segment->end_x_   >= subtraction.start_x_ &&
+            segment->start_y_ <= subtraction.end_y_   &&
+            segment->end_y_   >= subtraction.start_y_ &&
+            segment->start_z_ <= subtraction.end_z_   &&
+            segment->end_z_   >= subtraction.start_z_) {
+          if (segment->start_x_ < subtraction.start_x_) {
+            Range x_slice_1 = Range {
+              segment->start_x_, subtraction.start_x_ - 1,
+              segment->start_y_, segment->end_y_,
+              segment->start_z_, segment->end_z_ };
+            segment->start_x_ = subtraction.start_x_;
+            new_segments.push_back(x_slice_1);
+          }
+          if (segment->end_x_ > subtraction.end_x_) {
+            Range x_slice_2 = Range {
+              subtraction.end_x_ + 1, segment->end_x_,
+              segment->start_y_, segment->end_y_,
+              segment->start_z_, segment->end_z_ };
+            segment->end_x_ = subtraction.end_x_;
+            new_segments.push_back(x_slice_2);
+          }
+          if (segment->start_y_ < subtraction.start_y_) {
+            Range y_slice_1 = Range {
+              segment->start_x_, segment->end_x_,
+              segment->start_y_, subtraction.start_y_ - 1,
+              segment->start_z_, segment->end_z_ };
+            segment->start_y_ = subtraction.start_y_;
+            new_segments.push_back(y_slice_1);
+          }
+          if (segment->end_y_ > subtraction.end_y_) {
+            Range y_slice_2 = Range {
+              segment->start_x_, segment->end_x_,
+              subtraction.end_y_ + 1, segment->end_y_,
+              segment->start_z_, segment->end_z_ };
+            segment->end_y_ = subtraction.end_y_;
+            new_segments.push_back(y_slice_2);
+          }
+          if (segment->start_z_ < subtraction.start_z_) {
+            Range z_slice_1 = Range {
+              segment->start_x_, segment->end_x_,
+              segment->start_y_, segment->end_y_,
+              segment->start_z_, subtraction.start_z_ - 1 };
+            segment->start_z_ = subtraction.start_z_;
+            new_segments.push_back(z_slice_1);
+          }
+          if (segment->end_z_ > subtraction.end_z_) {
+            Range z_slice_2 = Range {
+              segment->start_x_, segment->end_x_,
+              segment->start_y_, segment->end_y_,
+              subtraction.end_z_ + 1, segment->end_z_ };
+            segment->end_z_ = subtraction.end_z_;
+            new_segments.push_back(z_slice_2);
+          }
+          segments_.erase(segment++);
+        }
+        else {
+          ++segment;
+        }
+      }
+      segments_.splice(segments_.end(), new_segments);
+    }
+    
+    void add_segment(const Range& addition) {
+      subtract_segment(addition);
+      segments_.push_back(addition);
+    }
+    
+    long long power()
+    {
+      return std::accumulate(segments_.begin(), segments_.end(), 0ll,
+                             [](long long total, const Range& item)
+                             {
+                                return total + (item.end_x_ - item.start_x_ + 1) *
+                                               (item.end_y_ - item.start_y_ + 1) *
+                                               (item.end_z_ - item.start_z_ + 1);
+                             }
+      );
+    }
+    
+  private:
+    std::list<Range> segments_;
+  };
+}
+
+int main()
+{
+  std::ifstream input("AoC_22_input.dat");
+  if (input.fail()) {
+    std::cout << "Didn't find the input file\n";
+    return 1;
+  }
+  
+  int x = 0;
+  Engine engine;
+  do {
+    char buf[5000];
+    input.getline(buf, 5000);
+    std::stringstream string_input(buf);
+    std::string on_off;
+    char xc, yc, zc, equals, dot, comma;
+    Range this_range;
+    string_input >> on_off >>
+                    xc >> equals >> this_range.start_x_ >> dot >> dot >> this_range.end_x_ >> comma >>
+                    yc >> equals >> this_range.start_y_ >> dot >> dot >> this_range.end_y_ >> comma >>
+                    zc >> equals >> this_range.start_z_ >> dot >> dot >> this_range.end_z_;
+    if (on_off == "on") {
+      engine.add_segment(this_range);
+    }
+    else {
+      engine.subtract_segment(this_range);
+    }
+    if (++x == 20) {
+      std::cout << "Part One: " << engine.power() << "\n";
+    }
+  } while (!input.eof());
+  
+  std::cout << "Part Two: " << engine.power() << "\n";
+  
+  return 0;
+}
